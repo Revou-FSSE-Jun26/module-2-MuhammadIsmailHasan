48abd835945fbf1b85cce7f7716abe4000f5dd6f
Ismail <ismailhas22@gmail.com>
2026-07-31 06:16:04 +0700

    feat(schema): add relationship between table

--- schema.sql
+++ schema.sql
@@ -11,7 +11,10 @@ CREATE TABLE products (
     description VARCHAR(1000),
     price NUMERIC(11, 2) NOT NULL,
     stock INTEGER NOT NULL DEFAULT 0,
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_products_category 
+        FOREIGN KEY (category_id) REFERENCES categories(id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE users (
@@ -27,13 +30,22 @@ CREATE TABLE orders (
     user_id INTEGER NOT NULL,
     total_amount NUMERIC(14, 2) NOT NULL,
     status VARCHAR(25) NOT NULL DEFAULT 'waitingForPayment',
-    ordered_at TIMESTAMP DEFAULT NOW()
+    ordered_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT fk_orders_user 
+        FOREIGN KEY (user_id) REFERENCES users(id)
+        ON DELETE RESTRICT
 );
 
 CREATE TABLE order_items (
-    id SERIAL PRIMARY KEY,
     order_id INTEGER NOT NULL,
     product_id INTEGER NOT NULL,
-    price_ordered NUMERIC(14, 2) NOT NULL
-    discount NUMERIC(3, 2)
+    price_ordered NUMERIC(14, 2) NOT NULL,
+    discount NUMERIC(3, 2),
+    PRIMARY KEY (order_id, product_id),
+    CONSTRAINT fk_oi_order 
+        FOREIGN KEY (order_id)  REFERENCES orders(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_oi_product
+        FOREIGN KEY (product_id) REFERENCES products(id)
+        ON DELETE RESTRICT
 );
