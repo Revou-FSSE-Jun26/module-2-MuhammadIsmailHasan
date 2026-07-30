ffa98accf9aa7c8191da761999ddd532387da0a3
Ismail <ismailhas22@gmail.com>
2026-07-30 15:57:27 +0700

    feat(db) : create table schema

---
+++ schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    category_id INTEGER NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(1000),
+    price NUMERIC(11, 2) NOT NULL,
+    stock_quantity INTEGER NOT NULL DEFAULT 0,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE categories {
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT NOW()
+}
+
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    total_amount NUMERIC(14, 2) NOT NULL,
+    status VARCHAR(25) NOT NULL DEFAULT 'waitingForPayment',
+    ordered_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE order_items {
+    id SERIAL PRIMARY KEY,
+    order_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    price_ordered NUMERIC(14, 2) NOT NULL
+    discount NUMERIC(3, 2)
+}
