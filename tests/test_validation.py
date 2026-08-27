a80f4373d5fe347c7ac8abaf6b785246f35114e2
Ismail <ismailhas22@gmail.com>
2026-08-28 06:22:03 +0700

    test: reorganize integration tests into dedicated directory

--- tests/test_validation.py
+++
@@ -1,9 +0,0 @@
-from app.validation import (
-    validation_category_data,
-    validation_products_data,
-    validation_users_data,
-    validation_order_data,
-    validation_order_status,
-    validation_delete_order,
-    is_valid_email,
-)
